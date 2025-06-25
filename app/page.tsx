fa2cdf10738730db4f344f91e356bee06ceb63b5
Running water <alsgks1218@naver.com>
2025-06-25 13:34:16 +0900

    웹뷰 디버깅을 위해 버튼 코드 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-amber-400 flex items-center justify-center flex-col">
       <h1 className="text-2xl">안녕하세요</h1>
       <p>웹뷰입니다.</p>
+      <button
+        className="bg-emerald-400 p-2"
+        onClick={() => console.log("웹 클릭입니다.")}
+      >
+        웹뷰 버튼
+      </button>
     </div>
   );
 }
